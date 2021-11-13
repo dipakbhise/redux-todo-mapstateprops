7f7f2b4b9c9e55e527651cae19ab5f9963c29858
Dipak Bhise <dipakbhise420@gmail.com>
2021-11-13 10:54:33 +0530

    Update FilterLink.js

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -7,7 +7,7 @@ const mapStateToProps = (state, ownProps) => ({
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: ()=>dispatch(setVisibilityFilter(ownProps.filter))
+  onClick: (ownProps.filter)=>dispatch(setVisibilityFilter(ownProps.filter))
 })
 
 export default connect(
